a9a083ca49b06ca6e4ebf48ee78a39bc99582744
Andressa Abrantes <andressasabrantes@gmail.com>
2023-08-04 20:45:17 -0300

    Dependency Injenction e Listagem de contatos

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -1,9 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
+using ProjetoMVC.Repositories;
+
 
 namespace ProjetoMVC.Controllers
 {
     public class ContatoController : Controller
     {
+        private readonly AgendaRepository _dbcontext;
+
+        public ContatoController(AgendaRepository dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
         public IActionResult Index()
         {
             return View();
