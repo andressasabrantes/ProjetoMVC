199e6389aec84b3d06f5fc9a22e8d8ac7289ca68
Andressa Abrantes <andressasabrantes@gmail.com>
2023-08-04 22:38:16 -0300

    Método criar() com Post implementado

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using ProjetoMVC.Models;
 using ProjetoMVC.Repositories;
 
 namespace ProjetoMVC.Controllers
@@ -13,12 +14,25 @@ namespace ProjetoMVC.Controllers
         }
         public IActionResult Index()
         {
-            return View();
+            var contatos = _dbcontext.Contatos.ToList();
+            return View(contatos);
         }
 
         public IActionResult Criar()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Contato contato)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbcontext.Contatos.Add(contato);
+                _dbcontext.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contato);
         }
     }
 }
